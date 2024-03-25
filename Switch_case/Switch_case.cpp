a1cf7b3d559f01ecd0c20077a0ec86f6274be5e0
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-03-25 10:32:30 +0700

    SELESAI

--- Switch_case/Switch_case.cpp
+++ Switch_case/Switch_case.cpp
@@ -71,7 +71,14 @@ do {
         cout << "Hasil pembagian = " << Pembagian(a, b) << endl;
         system("pause");
         break;
-    
+    case 5:
+        break;
+    default:
+        cout << "Menu tidak tersedia silahkan pilih lagi !!!" << endl;
+        break;
+    }
+
+} while (pilihan != 5);
 
 
 
